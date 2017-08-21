0bd3df12e824c86ee65d61289683c8fb6f018be1
Okadi <leethaker12346@gmail.com>
2017-08-21 01:23:04 +0100

    does it work now

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./files/config.json')
+const token = require('./files/token.json')
 
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if(message.content === 'pingu') {
+        message.reply('pongu');
+    }
+});
+
+client.login(token.token);
