4f94808ecf91fd0556538acc023b77c25d659f66
Okadi <leethaker12346@gmail.com>
2017-08-21 19:51:59 +0100

    hi

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const client = new Discord.Client()
 const config = require('./files/config.json')
 const token = require('./files/token.json')
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Nathan has entered the building. \nYou quake in your boots.')
+    
+    client.user.setStatus("online")
+    client.user.setPresence({ game: { name: 'with my students', type: 0 } })
 });
 
 client.on('message', message => {
-    if(message.content === 'pingu') {
-        message.reply('pongu');
+    if(message.content === '!ping') {
+        message.reply('pong!')
     }
 });
 
-client.login(token.token);
+client.login(token.token)
