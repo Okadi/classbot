654990fbd062b43d9d99670a891626c47b84ecbf
Okadi <leethaker12346@gmail.com>
2017-08-25 00:19:37 +0100

    yo

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if(message.content === '!ping') {
-        message.reply('pong!')
+    if(message.content === '!ping' && !message.author.bot) {
+        message.channel.send('!ping')
     }
 });
 
