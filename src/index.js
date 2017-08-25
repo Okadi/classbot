b4b974a1f3a58bc55fade6a725a0d6bc81e7bc9b
Okadi <leethaker12346@gmail.com>
2017-08-25 02:40:04 +0100

    lets go dudes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const colors = require('colors');
 const config = require('./files/config.json')
 const token = require('./files/token.json')
 
 client.on('ready', () => {
-    console.log('Nathan has entered the building. \nYou quake in your boots.')
+    console.log('Nathan has entered the building.\nYou quake in your boots.\n\nBot successfully started!\n\nMessage log:\n')
     
     client.user.setStatus("online")
     client.user.setPresence({ game: { name: 'with my students', type: 0 } })
@@ -13,6 +14,9 @@ client.on('ready', () => {
 client.on('message', message => {
     if(message.content === '!ping' && !message.author.bot) {
         message.channel.send('!ping')
+        console.log(message.author.username.green.bold+" in #".green.bold+message.channel.name.green.bold+": ".green.bold+message.content);
+    } else {
+        console.log(message.author.username.red.bold+" in #".red.bold+message.channel.name.red.bold+": ".red.bold+message.content);
     }
 });
 
